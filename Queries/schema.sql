2baaf843ca53015da4f8326a21356c0eb88ed36e
Linda Berez <lberez@Lindas-iMac.attlocal.net>
2021-04-30 14:08:30 -0500

    Adding challenge query file

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -13,8 +13,7 @@ CREATE TABLE employees (
      last_name VARCHAR NOT NULL,
      gender VARCHAR NOT NULL,
      hire_date DATE NOT NULL,
-     PRIMARY KEY (emp_no)
-);
+     PRIMARY KEY (emp_no));
 
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
@@ -23,8 +22,7 @@ CREATE TABLE dept_manager (
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
-);
+    PRIMARY KEY (emp_no, dept_no));
 
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
@@ -32,7 +30,7 @@ CREATE TABLE salaries (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no))
+	PRIMARY KEY (emp_no));
 
 CREATE TABLE dept_employee (
 	dept_no VARCHAR(4) NOT NULL,
@@ -41,15 +39,12 @@ CREATE TABLE dept_employee (
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no))
-
+    PRIMARY KEY (emp_no, dept_no));
+    
 CREATE TABLE titles (
     emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no))
-
-
-
+	PRIMARY KEY (emp_no));
