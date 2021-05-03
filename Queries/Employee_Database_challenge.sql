e39a7e885944abbb2a8fbf12f3fa61925ed4095e
Linda Berez <lberez@Lindas-iMac.attlocal.net>
2021-05-02 20:30:38 -0500

    Adding csv files created for deliverable 3

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -11,7 +11,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no
 
---SELECT * FROM retirement_titles
+-- SELECT * FROM retirement_titles
 
 
 SELECT DISTINCT ON (emp_no) emp_no,
@@ -22,7 +22,7 @@ INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
---SELECT * FROM unique_titles
+-- SELECT * FROM unique_titles
 
 
 SELECT COUNT(title), title
@@ -31,4 +31,22 @@ FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
 
---SELECT * FROM retiring_titles
+-- SELECT * FROM retiring_titles
+
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title	
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31');
+
+-- SELECT * FROM mentorship_eligibilty
